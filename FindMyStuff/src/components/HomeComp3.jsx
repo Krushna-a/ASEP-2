5701da16fe72c200a496f022cd428fe7e9a17bf2
Krushna-a <Krushnaboinwad70@gmail.com>
2025-05-02 02:52:32 +0530

    Improved Styling of Home Page

--- FindMyStuff/src/components/HomeComp3.jsx
+++ FindMyStuff/src/components/HomeComp3.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const HomeComp3 = () => {
   return (
-    <div>
-      
+    <div className='w-full'>
+      <h1 className="text-2xl italic font-bold text-center my-10 bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent loadAnimation">
+        How It Works ?
+      </h1>
     </div>
   )
 }
